e88b84e28f71c6ab872817b3c692fdf3b6d7ab84
Tomer Keren <tomer.keren.dev@gmail.com>
2018-10-03 13:11:44 +0300

    :sparkles: Add a function to create a feature branch
    Sarting from this commit, you too can have this horrible, ugly function
    and branch names that are way too long

--- .functions.sh
+++ .functions.sh
@@ -1,3 +1,7 @@
 markdown_preview(){
 	markdown_py $1 | lynx -dump -stdin
 }
+
+git_feature_branch() {
+    ( [ -z "$2" ] && echo $2 ) || clippaste|python3 -c "print('_'.join(input().split()) + '$1')"|xargs git checkout -b
+}
