633fc34f04791302cc88a5c64dce7017140a9977
Tomer Keren <tomer.keren.dev@gmail.com>
2018-11-20 20:12:41 +0200

    :sparkles:Add function to quickly create a venv

--- .functions.sh
+++ .functions.sh
@@ -6,3 +6,14 @@ git_feature_branch() {
     # Adds a new git branch named [name_with_underscores_$1]. name is either in clipboard or given as a second argument
     (( [ "$2" ] && echo $2 ) || clippaste )|python3 -c "print('_'.join(input().split()) + '_$1')"|xargs git checkout -b
 }
+
+current_dir(){
+    # Prints the dirname (final part) of the cwd
+    echo ${PWD##*/}
+}
+
+init_venv(){
+    # Creates a py3 venv in a directory called venv with the cli prompt of the cwd's dirname
+    python3 -m venv venv --prompt $(current_dir)
+    source venv/bin/activate
+}
