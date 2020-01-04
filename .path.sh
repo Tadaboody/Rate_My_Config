c3f39ff051f8aea2a31a448ada56ea875436d27b
Tomer Keren <tomer.keren.dev@gmail.com>
2020-01-04 23:32:31 +0200

    :art: Use a function to add to path

--- .path.sh
+++ .path.sh
@@ -1,7 +1,11 @@
+function _path_append(){
+    export PATH="$PATH:$1"
+}
+ 
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH="/usr/local/opt/mysql-client/bin:$PATH"
-export PATH="/Users/tomer/Library/Python/3.6/bin:$PATH"
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+_path_append "/usr/local/opt/mysql-client/bin"
+_path_append "/Users/tomer/Library/Python/3.6/bin"
+_path_append "$ANDROID_HOME/emulator"
+_path_append "$ANDROID_HOME/tools"
+_path_append "$ANDROID_HOME/tools/bin"
+_path_append "$ANDROID_HOME/platform-tools"
