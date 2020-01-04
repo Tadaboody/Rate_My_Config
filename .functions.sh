f85cfbe8af17d179ff8c299e7b1257fcc20d52d8
Tomer Keren <tomer.keren.dev@gmail.com>
2020-01-04 23:32:45 +0200

    Add directories to ~/.path.sh by function

--- .functions.sh
+++ .functions.sh
@@ -2,11 +2,6 @@ markdown_preview(){
 	markdown_py $1 | lynx -dump -stdin
 }
 
-git_feature_branch() {
-    # Adds a new git branch named [name_with_underscores_$1]. name is either in clipboard or given as a second argument
-    (( [ "$2" ] && echo $2 ) || clippaste )|python3 -c "print('_'.join(input().split()) + '_$1')"|xargs git checkout -b
-}
-
 current_dir(){
     # Prints the dirname (final part) of the cwd
     echo ${PWD##*/}
@@ -42,5 +37,6 @@ add_to_path() {
         echo "Argument isn't a directory, not adding"
         ret 1
     fi
-    echo "export PATH=$1;$PATH" >>.path
+    echo "_path_append $(realpath $1)" >>~/.path.sh
+    source ~/.path.sh
 }
